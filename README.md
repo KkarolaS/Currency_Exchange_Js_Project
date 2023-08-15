6ce284ca5b59fd006e45098ec5c9cc571ffe1df5
KkarolaS <133031923+KkarolaS@users.noreply.github.com>
2023-08-15 15:37:42 +0200

    create html, css and js code for simple currency exchanger

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Currency_Exchange_Js_Project
+
+In progress.....
+
+<!-- ## Welcome to my website project (HTML + CSS + JS)
+
+Hi I'm Caroline and here you can find my second JS project.
+
+Below you can find some link to project's repository in GitHub and also link to Netlify.
+
+## Link to GitHub Repo
+
+Here you can find link to GitHup Repo:
+
+```bash
+
+```
+
+## Link to Netlify
+
+Here you can find link to Netlify Repo:
+
+```bash
+
+```
+
+## Authors and acknowledgment
+
+The project is a part of Bootcamp HTML&CSS with Future Collars and I'd not be here without all Mentors they're helping me during the learning process. -->
---
+++ index.js
@@ -0,0 +1,50 @@
+const select = document.querySelector("#currencies-options");
+const button = document.querySelector("#getCurrencies-btn");
+const input = document.querySelector("#input-amount");
+const resultInfo = document.querySelector("#currency-value");
+const loader = document.querySelector("#loader");
+
+let choosenCurrency = "";
+
+const getCurrency = async (code) => {
+  try {
+    loader.classList.remove("hidden");
+    const response = await axios.get(
+      `http://api.nbp.pl/api/exchangerates/rates/c/${code}/?format=json`
+    );
+    const { data } = response;
+    const { rates } = data;
+    const { ask } = rates[0];
+    return ask;
+  } catch (error) {
+    console.error(error);
+    alert("Wybierz walutę");
+  } finally {
+    setTimeout(() => {
+      loader.classList.add("hidden");
+    }, 400);
+    console.log("Done");
+  }
+};
+
+const multiplyValue = (currencyValue) => {
+  const result = input.value * currencyValue;
+  resultInfo.textContent = `${result} PLN`;
+};
+
+const main = async () => {
+  const currencyActualValue = await getCurrency(choosenCurrency);
+  multiplyValue(currencyActualValue);
+};
+
+select.addEventListener("change", (event) => {
+  choosenCurrency = event.target.value;
+});
+
+button.addEventListener("click", () => {
+  if (input.value && choosenCurrency !== "" && choosenCurrency !== NaN) {
+    main();
+  } else {
+    alert("Podaj wartość i wybierz walutę");
+  }
+});
